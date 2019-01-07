01b4da4ad96d878da3aae98fa8c81957ba4b41eb
Mina Mekhael <mina.mekhael@ymail.com>
2019-01-07 06:09:50 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,4 +1,5 @@
-attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]}
+attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
 def badge_maker(name) 
   puts 
   "Hello, my name is #{name}."
