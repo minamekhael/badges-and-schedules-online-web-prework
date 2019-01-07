547d3d962608272e8d2b3bebff8e3d7844a56e4e
Mina Mekhael <mina.mekhael@ymail.com>
2019-01-07 06:24:28 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -26,6 +26,5 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  assign_rooms(attendees)
-  batch_badge_creator(attendees)
+ puts assign_rooms(attendees), batch_badge_creator(attendees)
 end
