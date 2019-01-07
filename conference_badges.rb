42ce4579380388a6827225b9f9159dce6a8e7783
Mina Mekhael <mina.mekhael@ymail.com>
2019-01-07 06:28:58 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,10 +1,8 @@
 attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
-def badge_maker(name) 
-  puts 
+def badge_maker(name)
   "Hello, my name is #{name}."
 end
-
 def batch_badge_creator(attendees)
   attendees.collect do |name|
     badge_maker(name)
